1eb349a5ff44820a0efcea77afaaac34663c30a3
Kenny Do <kedo@ocf.berkeley.edu>
2015-03-28 14:04:16 -0700

    added tmux installable

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ case "$1" in
         ln -s `readlink -f bash/.bash_profile` ~/
         ln -s `readlink -f bash/.bashrc` ~/
         ;;
+    tmux)
+        echo "Installing tmux"
+        ln -s `readlink -f tmux/.tmux.conf` ~/
+        ;;
     *)
         echo $"Unrecognized installable: $1"
         exit 1
