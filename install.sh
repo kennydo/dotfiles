230f312eabcac070fb5bb4da2549a22858b87093
Kenny Do <kennyhdo@gmail.com>
2015-03-28 14:14:18 -0700

    moved git's install script into the global install script

--- install.sh
+++ install.sh
@@ -6,6 +6,29 @@ case "$1" in
         ln -s `readlink -f bash/.bash_profile` ~/
         ln -s `readlink -f bash/.bashrc` ~/
         ;;
+    git)
+        echo "Installing git"
+        if [ -z "`git config --global user.name`" ]; then
+            read -p "Name: " name
+            git config --global user.name "$name"
+        fi
+        if [ -z "`git config --global user.email`" ]; then
+            read -p "E-mail: " email
+            git config --global user.email "$email"
+        fi
+
+        cp `readlink -f git/.gitignore_global` ~/
+
+        git config --global core.editor "vim"
+        git config --global core.excludesfile ~/.gitignore_global
+
+        git config --global color.ui "auto"
+
+        git config --global push.default "matching"
+
+        git config --global alias.lg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
+        git config --global alias.lg2 "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(bold white)— %an%C(reset)' --abbrev-commit"
+        ;;
     mutt)
         echo "Installing mutt"
         ln -s `readlink -f mutt/.muttrc` ~/
