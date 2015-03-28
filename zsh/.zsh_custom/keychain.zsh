71f24c509ab9b391b0752e51eed8ce9408d79e6c
Kenny Do <kdo@linkedin.com>
2015-03-28 16:01:31 -0700

    removed debugging echo

--- zsh/.zsh_custom/keychain.zsh
+++ zsh/.zsh_custom/keychain.zsh
@@ -1,4 +1,4 @@
 if [[ -x /usr/bin/keychain ]] then
-    find ~/.ssh -type f -name '*_key' -exec echo /usr/bin/keychain {} \;
+    find ~/.ssh -type f -name '*_key' -exec /usr/bin/keychain {} \;
     source ~/.keychain/`hostname`-sh
 fi
