95fc1b61138ea31522bac355a0c5884beba7590e
Kenny Do <kdo@linkedin.com>
2015-04-27 14:37:13 -0700

    alias cal to make it always highlighted

--- zsh/.zsh_custom/aliases.zsh
+++ zsh/.zsh_custom/aliases.zsh
@@ -1,3 +1,5 @@
 alias grep='grep --color=auto'
 alias sl="ls"
 
+# cal on Mac doesn't highlight by default
+alias cal='cal | GREP_COLOR="1;31" grep --before-context 6 --after-context 6 --color -e " $(date +%e)" -e "^$(date +%e)"'
