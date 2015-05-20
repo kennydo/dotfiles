dec361870bf3a7983146b65640034e3356291289
Kenny Do <kdo@linkedin.com>
2015-05-20 14:51:11 -0700

    only make the cal alias on darwin

--- zsh/.zsh_custom/aliases.zsh
+++ zsh/.zsh_custom/aliases.zsh
@@ -1,5 +1,9 @@
 alias grep='grep --color=auto'
 alias sl="ls"
 
-# cal on Mac doesn't highlight by default
-alias cal='cal | GREP_COLOR="1;31" grep --before-context 6 --after-context 6 --color -e " $(date +%e)" -e "^$(date +%e)"'
+# cal on Mac doesn't highlight by default, but it works fine on Linux
+case $OSTYPE in
+    darwin*)
+        alias cal='/usr/bin/cal | GREP_COLOR="1;31" grep --before-context 6 --after-context 6 --color -e "\b$(date +%e)\b"'
+        ;;
+esac
