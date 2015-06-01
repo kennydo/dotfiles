1a739b11ab003781b9e1d4f8a2728d5bff3db8ac
Kenny Do <kdo@linkedin.com>
2015-06-01 15:31:55 -0700

    installed gcal from homebrew

--- zsh/.zsh_custom/aliases.zsh
+++ zsh/.zsh_custom/aliases.zsh
@@ -4,6 +4,6 @@ alias sl="ls"
 # cal on Mac doesn't highlight by default, but it works fine on Linux
 case $OSTYPE in
     darwin*)
-        alias cal='/usr/bin/cal | GREP_COLOR="1;31" grep --before-context 6 --after-context 6 --color -e "\b$(date +%e)\b"'
+        alias cal='/usr/local/bin/gcal'
         ;;
 esac
