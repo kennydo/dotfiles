aa84e8059c712fae2f8e0b8475c7e979bc543706
Kenny Do <kdo@linkedin.com>
2015-10-09 10:50:25 -0700

    add some git aliases

--- install.sh
+++ install.sh
@@ -34,6 +34,10 @@ case "$1" in
 
         git config --global alias.lg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
         git config --global alias.lg2 "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(bold white)— %an%C(reset)' --abbrev-commit"
+        git config --global alias.co checkout
+        git config --global alias.br branch
+        git config --global alias.ci commit
+        git config --global alias.st status
         ;;
     mutt)
         echo "Installing mutt"
