e53e67b4ebee1f89fc4bf578abb5fa71916be35e
Kenny Do <kedo@patreon.com>
2016-04-29 13:53:17 -0700

    Add cleanup-merged-branches alias to git

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ case "$1" in
 
         git config --global alias.lg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
         git config --global alias.lg2 "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(bold white)— %an%C(reset)' --abbrev-commit"
+        git config --global alias.cleanup-merged-branches "!git branch --merged | grep -v '\\*\\|master\\|release' | xargs -n 1 git branch -d"
         git config --global alias.co checkout
         git config --global alias.br branch
         git config --global alias.ci commit
