f2596eafeabd5eee6c039f9bb2f144ed1423d024
Kenny Do <kedo@patreon.com>
2016-05-04 15:48:25 -0700

    Update git push.default to "simple"

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ case "$1" in
 
         git config --global color.ui "auto"
 
-        git config --global push.default "matching"
+        git config --global push.default "simple"
 
         git config --global alias.lg "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
         git config --global alias.lg2 "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(bold white)— %an%C(reset)' --abbrev-commit"
