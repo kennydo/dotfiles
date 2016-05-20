dc69a63b8d471e2efd590b9f8da0e9915377b2f0
Kenny Do <kedo@patreon.com>
2016-05-20 12:53:19 -0700

    Update push alias to not specify master

--- git/configure_aliases.sh
+++ git/configure_aliases.sh
@@ -10,4 +10,4 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 
-git config --global alias.pomf "push origin master --force-with-lease"
+git config --global alias.pof "push origin --force-with-lease"
