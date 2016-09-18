c396cc7fbd860d6afd344d900867ee6228e4ccda
Kenny Do <kedo@patreon.com>
2016-09-17 21:45:02 -0700

    Remove virtualenvwrapper

--- zsh/.zsh_custom/python.zsh
+++ zsh/.zsh_custom/python.zsh
@@ -1,18 +1,3 @@
-# Set up virtualenv wrapper
-wrapper_paths=(
-    /usr/share/virtualenvwrapper/virtualenvwrapper.sh # ubuntu/debian puts it here
-    /etc/bash_completion.d/virtualenvwrapper # debian wheezy puts it here
-    /usr/bin/virtualenvwrapper.sh
-    /usr/local/bin/virtualenvwrapper.sh
-)
-for f in $wrapper_paths; do
-    if [ -e $f ]; then
-        source $f
-        break
-    fi
-done
-unset wrapper_paths, f
-
 export WORKON_HOME=$HOME/.virtualenvs
 
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
