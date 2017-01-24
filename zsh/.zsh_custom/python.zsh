1e14dd7c6866781ca9ce76531eb37ac5d7c0127d
Kenny Do <chinesedewey@gmail.com>
2017-01-24 04:06:11 -0800

    Add pyenv

--- zsh/.zsh_custom/python.zsh
+++ zsh/.zsh_custom/python.zsh
@@ -1,3 +1,9 @@
 export WORKON_HOME=$HOME/.virtualenvs
 
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+export PYENV_ROOT="$HOME/.pyenv"
+
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
