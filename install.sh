0590b6df92b61fea9a26ae4629012f596cd692f1
Kenny Do <kedo@patreon.com>
2017-04-19 03:28:18 -0700

    Remove stale atom package list

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ case "$1" in
         echo "Installing atom config"
         ln -s $PWD/atom/.atom ~/
         cp $PWD/atom/.atom/config.cson{.dist,}
-        apm install --packages-file atom/package_list.txt
         ;;
     bash)
         echo "Installing bash"
