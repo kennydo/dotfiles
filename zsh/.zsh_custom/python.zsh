2cf29bbb8c324a1e3c6c69e5b1d9a345c668c5e1
Kenny Do <chinesedewey@gmail.com>
2017-11-17 19:23:40 -0800

    Conditional init of pyenv

--- zsh/.zsh_custom/python.zsh
+++ zsh/.zsh_custom/python.zsh
@@ -2,8 +2,9 @@ export WORKON_HOME=$HOME/.virtualenvs
 
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 
-export PYENV_ROOT="$HOME/.pyenv"
+if [[ -d "$HOME/.pyenv" ]]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
 
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-eval "$(pyenv init -)"
