994b3e4d672f10f1b83c6dd52858cdc54758e772
Kenny Do <kedo@yelp.com>
2019-07-18 17:12:33 -0700

    Add function to print git command to run to clean up remote branches

--- zsh/.zsh_custom/git.zsh
+++ zsh/.zsh_custom/git.zsh
@@ -1,3 +1,7 @@
 export PATH=~/.git_commands:$PATH
 
 alias groot='cd $(git rev-parse --show-toplevel)'
+
+print-git-remote-branch-pruning-command() {
+    echo 'git push origin' $(git branch --remote --list '*/kedo/*' | sed 's/^\s*origin\//:/' | tr '\n' ' ')
+}
