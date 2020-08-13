e1b8940b9f185cef884aa2742751cb4fcfd76e06
Kenny Do <kedo@yelp.com>
2020-08-12 17:54:51 -0700

    Automatically prune branches when fetching

--- git/configure_ux.sh
+++ git/configure_ux.sh
@@ -4,6 +4,8 @@ git config --global core.editor "vim"
 
 git config --global color.ui "auto"
 
+git config --global fetch.prune "true"
+
 git config --global push.default "simple"
 
 git config --global diff.compactionHeuristic "true"
