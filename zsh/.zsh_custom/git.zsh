84be1e95f2ba95a5f9ca0629e6879885549bf8ec
Kenny Do <kedo@yelp.com>
2020-09-16 14:47:34 -0700

    Add function to generate empty VS Code workspace at the git repo root

--- zsh/.zsh_custom/git.zsh
+++ zsh/.zsh_custom/git.zsh
@@ -5,3 +5,7 @@ alias groot='cd $(git rev-parse --show-toplevel)'
 print-git-remote-branch-pruning-command() {
     echo 'git push origin' $(git branch --remote --list '*/kedo/*' | sed 's/^\s*origin\//:/' | tr '\n' ' ')
 }
+
+generate-code-workspace() {
+    echo '{"folders":[{"path": "."}], "settings": {}}' > $(git rev-parse --show-toplevel)/workspace.code-workspace
+}
