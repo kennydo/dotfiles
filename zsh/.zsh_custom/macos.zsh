aa4a2e99a7c03e1f79c04f2fed835db93577dad6
Kenny Do <chinesedewey@gmail.com>
2022-07-20 20:08:53 -0700

    Add homebrew to path on mac

---
+++ zsh/.zsh_custom/macos.zsh
@@ -0,0 +1,7 @@
+if [[ -n "$__CFBundleIdentifier" ]]; then
+    # Janky check to see we're on mac
+
+    if $(type /opt/homebrew/bin/brew &> /dev/null); then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+fi
