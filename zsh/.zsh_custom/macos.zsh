9e217339429c937814c7be39106730aeb46aabdd
Kenny Do <chinesedewey@gmail.com>
2023-02-17 18:01:21 -0800

    Include homebrew dir in FPATH for autocompletion

--- zsh/.zsh_custom/macos.zsh
+++ zsh/.zsh_custom/macos.zsh
@@ -4,4 +4,6 @@ if [[ -n "$__CFBundleIdentifier" ]]; then
     if $(type /opt/homebrew/bin/brew &> /dev/null); then
         eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
+
+    FPATH="/opt/homebrew/share/zsh/site-functions:${FPATH}"
 fi
