d3fd6bfb85811356bd4d2c8fea60de87fbb29285
Kenny Do <chinesedewey@gmail.com>
2023-09-27 02:09:23 -0700

    Add back fzf

--- zsh/.zsh_custom/search.zsh
+++ zsh/.zsh_custom/search.zsh
@@ -1,3 +1,6 @@
+# Load fzf config first so that its Ctrl-R keybinding is overridden.
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 local FOUND_ATUIN=$+commands[atuin]
 
 if [[ $FOUND_ATUIN -eq 1 ]]; then
