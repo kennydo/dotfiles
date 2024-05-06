b07240cc658aead0b33eb6757521bf88ba73d8bf
Kenny Do <chinesedewey@gmail.com>
2024-05-05 20:54:41 -0700

    Set some more git config options

--- git/configure_ux.sh
+++ git/configure_ux.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
+git config --global color.ui auto
+git config --global core.editor vim
+git config --global diff.compactionHeuristic true
+git config --global fetch.prune true
 git config --global init.defaultBranch main
-
-git config --global core.editor "vim"
-
-git config --global color.ui "auto"
-
-git config --global fetch.prune "true"
-
-git config --global push.default "simple"
-
-git config --global diff.compactionHeuristic "true"
-
-git config --global pull.ff "only"
+git config --global merge.conflictstyle zdiff3
+git config --global pull.ff only
+git config --global push.default simple
+git config --global push.autoSetupRemote true
