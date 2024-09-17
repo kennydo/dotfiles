90d4ac86318b0f6186f7ed0976ae92905ecd5cec
Kenny Do <chinesedewey@gmail.com>
2024-09-16 23:23:11 -0700

    use uv for python

--- zsh/.zsh_custom/python.zsh
+++ zsh/.zsh_custom/python.zsh
@@ -1,10 +1,4 @@
-export WORKON_HOME=$HOME/.virtualenvs
-
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-
-if [[ -d "$HOME/.pyenv" ]]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-fi
+# uv
+[ -f ~/.cargo/env ] && source ~/.cargo/env
+export PATH="~/.local/bin:$PATH"
 
